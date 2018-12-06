b287304d8aa5d51f4d37a2602745c1e673d5bfe7
Charles Moulliard <cmoulliard@redhat.com>
2018-12-06 11:14:21 +0100

    Use Web starter

--- src/main/java/com/acme/hellogateway/HelloGatewayApplication.java
+++ src/main/java/com/acme/hellogateway/HelloGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.garethwestern.test.hellogateway;
+package com.acme.hellogateway;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,16 +18,6 @@ public class HelloGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloGatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(r ->
-						r.path("/test")
-						.uri("http://google.com:80")
-				)
-				.build();
 	}
 
 	@RestController
